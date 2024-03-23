223cd741c91468c69fbdd39c7f7e0d37895ac74a
Joe Burke <joseph_burke@outlook.com>
2024-03-23 23:00:17 +0000

    fix: basic implementation of OpenAiClient class

--- api/config/application.rb
+++ api/config/application.rb
@@ -18,12 +18,16 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Load environment variables
+Dotenv.load
+
 module Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     config.autoload_paths << Rails.root.join('lib')
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
---
+++ api/lib/open_ai_client.rb
@@ -0,0 +1,36 @@
+class OpenAiClient
+  def initialize
+    api_key = ENV['OPEN_AI_API_KEY']
+    @client = OpenAI::Client.new(access_token: api_key)
+  end
+
+  # Takes text and returns a generated image URL
+  def get_image_url(text)
+    response = @client.images.generate(
+      parameters: {
+        prompt: text,
+        size: "256x256"
+      }
+    )
+
+    response.dig('data', 0, 'url')
+  end
+
+  # Takes text and returns a file name of the generated speech audio
+  def get_speech_audio_from_text(text)
+    random_voice = %w[alloy echo fable onyx nova shimmer].sample
+    response = @client.audio.speech(
+      parameters: {
+        model: "tts-1",
+        input: text,
+        voice: random_voice,
+        speed: rand(0.8..1.0).round(1)
+      }
+    )
+    
+    file_name = "tmp/speech_files/#{Time.now.to_i}.mp3"
+    File.binwrite(file_name, response)
+
+    file_name
+  end
+end
