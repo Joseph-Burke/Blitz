f3cdbc93765b805b0228cdcad9a5715bd5da1a7f
Joe Burke <joseph_burke@outlook.com>
2024-04-02 15:46:24 +0100

    fix: implement #valid_fields and #to_hash

--- api/app/models/card/fields.rb
+++ api/app/models/card/fields.rb
@@ -11,9 +11,15 @@ class Card
       @template.fields - @parameters.keys
     end
 
+    # Returns the fields that are both valid (present in the template) and filled
+    def valid_fields
+      @template.fields.select { |field| @parameters[field.downcase] }
+    end
+
+    # Returns a hash with the valid fields and their values in @parameters
     def to_hash
-      template.fields.each_with_object({}) do |field, hash|
-        hash[field] = parameters[field.downcase] unless parameters[field.downcase].nil?
+      valid_fields.each_with_object({}) do |field, hash|
+        hash[field] = @parameters[field.downcase]
       end
     end
   end
